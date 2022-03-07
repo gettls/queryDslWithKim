ca263eaaf51db3e6cd6245e7d46d28f6449502ac
gettls <gettls@daum.net>
2022-03-07 23:00:11 +0900

    .

--- study_querydsl/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ study_querydsl/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -7,20 +7,31 @@ import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+import lombok.RequiredArgsConstructor;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
+import static org.springframework.util.StringUtils.*;
+import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.*;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberJpaRepository {
 
 	private final EntityManager em;
-	private final JPAQueryFactory jpaQueryFactory;
+	private final JPAQueryFactory queryFactory;
 	
-	public MemberJpaRepository(EntityManager em) {
-		this.em = em;
-		this.jpaQueryFactory = new JPAQueryFactory(em);
-	}
+//	public MemberJpaRepository(EntityManager em, JPAQueryFactory queryFactory) {
+//		this.em = em;
+//		this.queryFactory = queryFactory;
+//	}
 	
 	public void save(Member member) {
 		em.persist(member);
@@ -35,6 +46,12 @@ public class MemberJpaRepository {
 		return em.createQuery("select m from Member m",Member.class)
 					.getResultList();
 	}
+
+	public List<Member> findAll_Querydsl(){
+		return queryFactory
+					.selectFrom(member)
+					.fetch();
+	}
 	
 	public List<Member> findByUsername(String username){
 		return em.createQuery("select m from Member m where m.username = :username", Member.class)
@@ -43,4 +60,74 @@ public class MemberJpaRepository {
 				
 	}
 	
+	
+	public List<Member> findByUsername_Querydsl(String username){
+		return queryFactory
+						.selectFrom(member)
+						.where(member.username.eq(username))
+						.fetch();
+		
+	}
+	
+	public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition){
+		BooleanBuilder builder = new BooleanBuilder();
+		if(hasText(condition.getUsername())) {
+			builder.and(member.username.eq(condition.getUsername()));
+		}
+		if(hasText(condition.getTeamName())) {
+			builder.and(team.name.eq(condition.getTeamName()));
+		}
+		if(condition.getAgeGoe() != null) {
+			builder.and(member.age.goe(condition.getAgeGoe()));
+		}
+		if(condition.getAgeLoe() != null) {
+			builder.and(member.age.loe(condition.getAgeLoe()));
+		}
+		
+		return queryFactory
+					.select(new QMemberTeamDto(
+							member.id.as("memberId"),
+							member.username,
+							member.age,
+							team.id.as("teamId"),
+							team.name.as("teamName")))
+					.from(member)
+					.leftJoin(member.team, team)
+					.where(builder)
+					.fetch();
+	}
+	
+	public List<MemberTeamDto> searchByMultiParams(MemberSearchCondition condition){
+		return queryFactory
+				.select(new QMemberTeamDto(
+						member.id.as("memberId"),
+						member.username,
+						member.age,
+						team.id.as("teamId"),
+						team.name.as("teamName")))
+				.from(member)
+				.leftJoin(member.team, team)
+				.where(
+						usernameEq(condition.getUsername()),
+						teamNameEq(condition.getTeamName()),
+						ageGoe(condition.getAgeGoe()),
+						ageLoe(condition.getAgeLoe()))
+				.fetch();
+	}
+
+	private BooleanExpression usernameEq(String username) {
+		return hasText(username) ? member.username.eq(username) : null;
+	}
+	
+	private BooleanExpression teamNameEq(String teamName) {
+		return hasText(teamName) ? team.name.eq(teamName) : null;
+	}
+	
+	private BooleanExpression ageGoe(Integer ageGoe) {
+		return ageGoe != null ? member.age.goe(ageGoe) : null;
+	}
+	
+	private BooleanExpression ageLoe(Integer ageLoe) {
+		return ageLoe != null ? member.age.loe(ageLoe) : null;
+	}
 }
